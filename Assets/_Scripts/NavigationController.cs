675b46018989ff6b76e6b5226fd8fa0344555ac3
Xavier Chen <xavier.chen2000@gmail.com>
2024-02-10 19:03:26 +0000

    Added initial implementation for automatic map navigation using NavMesh and A* algorithm (implicit).

---
+++ Assets/_Scripts/NavigationController.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.AI;
+using Logging;
+
+/// <summary>
+/// This module controls the navigation of an object
+/// For now, attach a navigation mesh agent to this controller.
+/// Everytime you click a position on the map, it resets the
+/// agent's target to that position.
+/// </summary>
+[Tooltip("Controls the agents navigation; sets target on mouse click")]
+public class NavigationController : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent agent_;
+    [SerializeField] private Vector3 destination_;
+    private Camera cam_;
+
+    public void Start()
+    {
+        cam_ = Camera.main;
+    }
+
+    public void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam_.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                destination_ = hit.point;
+                agent_.SetDestination(destination_);
+                XLogger.Log(Category.Input, "Mouse position: " + Input.mousePosition);
+                XLogger.Log(Category.Input, "Destination: " + destination_);
+            }
+        }
+    }
+}
