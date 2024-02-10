f8095221342c59d034361d3ef73e8447a3165a13
Qiaozhi <ql316@cam.ac.uk>
2024-02-10 14:28:43 +0000

    Focus Lens

---
+++ Assets/FocusLens.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class FocusLens : Optics
+{
+    [SerializeField] private int focusLength;
+    
+    private Vector3 GetFocus(Vector3 _normal)
+    {
+        return transform.position + _normal * focusLength;
+    }
+    public override void ConstructGraph(LaserInfo _inLaser, Vector3 _hitNormal, float _length, LayerMask _hitLayer)
+    {
+        inLasers.Add(_inLaser);
+        Vector3 start = _inLaser.endPosition;
+        Vector3 focus = GetFocus(-_hitNormal);
+        Vector3 direction = (focus - start).normalized;
+        direction.y = 0;
+        
+        var ray = new Ray(start, direction);
+        LaserInfo laserInfo = CastRay(ray, _length, _hitLayer);
+        laserInfo.lineRendererPrefab = _inLaser.lineRendererPrefab;
+        outLasers.Add(laserInfo);
+    }
+}
