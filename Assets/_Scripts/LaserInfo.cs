d0e46614687ddc0cc03927c1479e5a82ae340ef4
Qiaozhi <ql316@cam.ac.uk>
2024-02-10 11:40:44 +0000

    reworked laser and optics

---
+++ Assets/_Scripts/LaserInfo.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class LaserInfo
+{
+    public Vector3 startPosition;
+    public Vector3 endPosition;
+    public LineRenderer lineRendererPrefab;
+    
+    public float Length()
+    {
+        return (endPosition - startPosition).magnitude;
+    }
+    
+    // intensity
+}
