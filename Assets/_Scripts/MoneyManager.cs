9470c54dc6caa447c0dc5a70998e467c112a5b16
Qiaozhi <ql316@cam.ac.uk>
2024-02-11 14:14:38 +0000

    Money Manager

---
+++ Assets/_Scripts/MoneyManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class MoneyManager : MonoBehaviour
+{
+    [SerializeField] private int initialMoney;
+
+    private int currentMoney_;
+
+    private void Start()
+    {
+        currentMoney_ = initialMoney;
+    }
+
+    public void ChangeMoney(int _change)
+    {
+        currentMoney_ += _change;
+    }
+
+    public int GetMoney()
+    {
+        return currentMoney_;
+    }
+}
