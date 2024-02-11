517cd99325572d6b142359772b7a58646199de8f
Qiaozhi <ql316@cam.ac.uk>
2024-02-11 15:01:29 +0000

    Money Spawn

---
+++ Assets/_Scripts/EnemyDamagable.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class EnemyDamagable : Damagable
+{
+    public EnemyInfo enemyInfo;
+    public GameObject coinPrefab;
+    public float coinSpawnRandomness;
+
+    protected override void Death()
+    {
+        base.Death();
+        var amount = enemyInfo.enemyCoinReward;
+        for (int i = 0; i < amount; i++)
+        {
+            GameObject coinGO = Instantiate(coinPrefab);
+            Vector2 randomOffset = Random.insideUnitCircle * coinSpawnRandomness;
+            coinGO.transform.position = transform.position + new Vector3(randomOffset.x, 0, randomOffset.y);
+            var money = coinGO.GetComponent<AddMoneyScript>();
+            money.SetMoneyManager(MoneyManager.Instance);
+            money.SetMoneyToAdd(10);
+        }
+    }
+}
